5ab16dda5a7a04adcb48bc8eb301717f904698a9
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-16 05:21:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = 'Grace Hopper';
 const welcomeMessage = `Welcome to Flatbook,  + ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome,  + ${currentUser.slice(12)}!`;
+const shortGreeting = `Welcome,  + ${currentUser.slice(11)}!`;
