7bf23b62c4fa8830a5bd497d84dfc09b10177224
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-04-16 05:05:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.upper + '!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.upcase + '!';
 
 const shortGreeting = 'Welcome, ' + '!';
